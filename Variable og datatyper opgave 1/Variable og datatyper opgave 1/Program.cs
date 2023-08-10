be7adb417f699df59ec746436bdddda1f26b0ed6
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2023-08-10 10:09:38 +0200

    Add files via upload

---
+++ Variable og datatyper opgave 1/Variable og datatyper opgave 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_og_datatyper_opgave_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+            int length = 16;
+            double width = 5.1;
+            //Console.WriteLine(length * width);
+
+            int area = length * width;
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
